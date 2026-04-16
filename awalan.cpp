61f72c70983174c7aae097b7967b8e28a7dec39e
Hamdan Abi <hamdanabialmujahidin@gmail.com>
2026-04-16 08:53:35 +0700

    Add files via upload

---
+++ awalan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*tipe data = int, float, char, string, bool*/
+	//cin = digunakan untuk input, output menggunakan tanda >>
+	//cout = dugunakan untuk output, output menggunakan tanda <<
+	
+	int a=8;
+	char b='A';
+	string nama="Andi";
+	float c=1.2;
+	bool d=0;
+	cout<<"Tampilkan variabel a :"<<a<<endl;
+	cout<<"Tampilkan variabel b :"<<b<<endl;
+	cout<<"Tampilkan variabel nama :"<<nama<<endl;
+	cout<<"Tampilkan variabel c :"<<c<<endl;
+	cout<<"Tampilkan variabel d :"<<d<<endl;
+}
