7cf8e107feee69cde7bfc9f8c8ab7382e7744d0b
Hamdan Abi <hamdanabialmujahidin@gmail.com>
2026-04-16 08:54:13 +0700

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//operator : + , - , * , / , %
+	//cin = digunakan untuk input, output menggunakan tanda >>
+	//cout = dugunakan untuk output, output menggunakan tanda <<
+	
+	int a;
+	int b;
+	int hasil;
+	cout<<"Masukkan nilai a:";
+	cin>>a;
+	cout<<"Maukkan nilai b:";
+	cin>>b;
+	hasil = a+b;
+	cout<<"Hasil :"<<hasil;
+}
